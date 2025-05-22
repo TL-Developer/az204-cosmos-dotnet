ee89cc150ed3adfa5c352925f1feddc0cd1f1b5e
Tiago Lima <tiago.jlima.developer@gmail.com>
2025-05-22 14:34:46 -0300

    add project

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Azure.Cosmos;
+
+public class Program
+{
+  private static readonly string EndpointUri = "https://restfull-app-db.documents.azure.com:443/";
+  private static readonly string PrimaryKey = "xkv0568MoCB0Zbaq0EgMep0EtSd5DlWACFSheVasHNEHmYHjKawWEG70P4giBABWXm5p1xIOgw0UACDbQh8YGg==";
+
+  private CosmosClient cosmosClient;
+  private Database database;
+  private Container container;
+  private string databaseId = "az204Database";
+  private string containerId = "az204Container";
+
+  public static async Task Main(string[] args)
+  {
+    try
+    {
+      Console.WriteLine("Beginning operations...\n");
+      Program p = new Program();
+      await p.CosmosAsync();
+
+    }
+    catch (CosmosException de)
+    {
+      Exception baseException = de.GetBaseException();
+      Console.WriteLine("{0} error occurred: {1}", de.StatusCode, de);
+    }
+    catch (Exception e)
+    {
+      Console.WriteLine("Error: {0}", e);
+    }
+    finally
+    {
+      Console.WriteLine("End of program, press any key to exit.");
+      Console.ReadKey();
+    }
+  }
+
+  public async Task CosmosAsync()
+  {
+    // Create a new instance of the Cosmos Client
+    this.cosmosClient = new CosmosClient(EndpointUri, PrimaryKey);
+
+    // Runs the CreateDatabaseAsync method
+    await this.CreateDatabaseAsync();
+
+    // Run the CreateContainerAsync method
+    await this.CreateContainerAsync();
+  }
+
+  private async Task CreateDatabaseAsync()
+  {
+    // Create a new database using the cosmosClient
+    this.database = await this.cosmosClient.CreateDatabaseIfNotExistsAsync(databaseId);
+    Console.WriteLine("Created Database: {0}\n", this.database.Id);
+  }
+  
+  private async Task CreateContainerAsync()
+  {
+    // Create a new container
+    this.container = await this.database.CreateContainerIfNotExistsAsync(containerId, "/LastName");
+    Console.WriteLine("Created Container: {0}\n", this.container.Id);
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # az204-cosmos-dotnet
+
+dotnet add package Microsoft.Azure.Cosmos
+dotnet add package Microsoft.Azure.DocumentDB.Core
+dotnet add package Newtonsoft.Json
